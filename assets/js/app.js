d711d449a28c33e7a43066757a84516f639ac064
Arlette Miranda <arlette.miranda.ch@gmail.com>
2024-06-29 14:48:16 -0600

    Se carga revision

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+/* Se acomodan el querySelector con sintaxis de la clase
+se pone . antes de la clase */
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
+const $l = document.querySelector('.location'); /* Se agrega a HTML */
+
+/* Se crea funcion asincrona, se agrega try y catch */
+async function displayUser(username) { 
+  try{
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); /* Se pone json para convertir a objeto */
+    console.log(data);
+    $n.textContent = data.name; /* Se quitan comillas para traer el dato */
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (err) {   
+    handleError(err) /* Se llama a la funcion de error */
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.error(err); /* Se imprime error */
+  $n.textContent = `Algo salió mal: ${err}`/* Se agrega $ a n */
 }
 
 displayUser('stolinski').catch(handleError);
